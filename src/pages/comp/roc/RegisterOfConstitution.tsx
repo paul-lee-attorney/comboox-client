256c597926d4acba2b81b853fd51b5734da5633e
paul-lee-attorney <paul_lee0919@163.com>
2023-10-08 13:07:05 +0800

    update roc mod

--- src/pages/comp/roc/RegisterOfConstitution.tsx
+++ src/pages/comp/roc/RegisterOfConstitution.tsx
@@ -114,7 +114,6 @@ function RegisterOfConstitution() {
                   pathAs="/comp/roc/Sha" 
                   setFile={setFile}
                   setOpen={setOpen}
-                  getFilesList={getFilesList}
                 />
               )}
 
