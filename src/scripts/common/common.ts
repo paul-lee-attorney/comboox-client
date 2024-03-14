ad7c46670be065f1c45c0d3e8d2fc2e6c683b00d
paul-lee-attorney <paul_lee0919@163.com>
2024-03-14 18:12:05 +0800

    remove pages files

--- src/scripts/common/common.ts
+++
@@ -1,9 +0,0 @@
-import { waitForTransaction } from "@wagmi/core";
-import { HexType } from ".";
-
-export async function getReceipt(hash: HexType): Promise< any > {
-  const res = await waitForTransaction({
-    hash: hash
-  });
-  return res;
-}
