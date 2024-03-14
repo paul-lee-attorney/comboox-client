ac811b0c04600c87c967f79a7bd5f5415490d4e8
paul-lee-attorney <paul_lee0919@163.com>
2024-03-14 09:08:52 +0800

    upgraded GMM funcs

---
+++ src/app/app/comp/gmm/page.tsx
@@ -0,0 +1,77 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import { Paper, Stack, Toolbar } from "@mui/material";
+
+import { GetMotionsList } from "./read/GetMotionsList";
+
+import { CreateMotionOfGm } from "./write/CreateMotionOfGm";
+import { Motion, getMotionsList } from "../read/meetingMinutes";
+import { ApprovalFormOfMotion } from "./write/ApprovalFormOfMotion";
+import { CopyLongStrSpan } from "../../read/CopyLongStr";
+import { booxMap } from "../../read";
+import { useComBooxContext } from "../../_providers/ComBooxContextProvider";
+
+function GeneralMeetingMinutes() {
+
+  const { boox } = useComBooxContext();
+
+  const [ motionsList, setMotionsList ] = useState<Motion[]>();
+
+  const [ open, setOpen ] = useState(false);
+  const [ motion, setMotion ] = useState<Motion>();
+  const [ time, setTime ] = useState<number>(0);
+
+  const refresh = ()=>{
+    setTime(Date.now());
+  }
+
+  useEffect(()=>{
+    if (boox) {
+      getMotionsList(boox[booxMap.GMM]).then(
+        ls => setMotionsList(ls)
+      );
+    }
+  }, [boox, time]);
+  
+  return (
+    <Paper elevation={3} sx={{alignContent:'center', justifyContent:'center', p:1, m:1, maxWidth:1680, border:1, borderColor:'divider' }} >
+
+      <Stack direction="row" >
+
+        <Toolbar sx={{m:1, p:1, textDecoration:'underline'}}>
+          <h3>GMM - General Meeting Minutes</h3>
+        </Toolbar>
+
+        {boox && (
+          <CopyLongStrSpan title="Addr"  src={ boox[booxMap.GMM].toLowerCase() }  />
+        )}
+
+      </Stack>
+
+      <CreateMotionOfGm  refresh={refresh} />
+
+      {motionsList && (
+        <GetMotionsList 
+          list={motionsList} 
+          title="Motions List - General Meeting of Members" 
+          setMotion={setMotion}
+          setOpen={setOpen}
+        />
+      )}
+
+      {motion && boox && (
+        <ApprovalFormOfMotion 
+          minutes={boox[booxMap.GMM]}  
+          open={open} 
+          motion={motion} 
+          setOpen={setOpen} 
+          refresh={refresh} 
+        />
+      )}
+
+    </Paper>
+  );
+} 
+
+export default GeneralMeetingMinutes;
