8602ac5fb2eaf28eb781a091e833306ed1670e90
paul-lee-attorney <paul_lee0919@163.com>
2025-05-15 09:57:04 +0800

    remove local hardhat network from WagmiProvider

--- src/app/_providers/WagmiProvider.tsx
+++ src/app/_providers/WagmiProvider.tsx
@@ -19,7 +19,7 @@ type WagmiProviderType = {
 }
 
 export const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [ hardhat, arbitrumSepolia, arbitrum,  mainnet ],
+  [ arbitrumSepolia, arbitrum ],
   // [ hardhat ],
   [
     alchemyProvider({
